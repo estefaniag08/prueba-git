fb67a00996c3f192c2c4135bfbdd03fbafb88d43
Valen <ly.valentina2003@gmail.com>
2023-09-26 21:23:29 -0500

    añadi un adios

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,6 @@
 public class Launcher {
     public static void main(String[] args){
-        System.out.println("Hola mundo");
+        System.out.println("Adios");
         System.out.println("Hola Estefania");
     }
 }
